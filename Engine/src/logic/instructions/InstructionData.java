c1616cddfa65c7db8c3e9d5c796276307db900dd
Sean Berger <seanbe@mta.ac.il>
2025-08-25 21:33:09 +0300

    added raw data for the synthetic instructions

--- Engine/src/logic/instructions/InstructionData.java
+++ Engine/src/logic/instructions/InstructionData.java
@@ -4,7 +4,17 @@ public enum InstructionData {
     INCREASE("INCREASE", 1, InstructionType.BASIC),
     DECREASE("DECREASE", 1, InstructionType.BASIC),
     JUMP_NOT_ZERO("JNZ", 3, InstructionType.BASIC),
-    NO_OP("NO_OP", 0, InstructionType.BASIC);
+    NO_OP("NO_OP", 0, InstructionType.BASIC),
+
+    ZERO_VARIABLE("ZERO_VARIABLE", 1, InstructionType.SYNTHETIC),
+    GOTO_LABEL("GOTO_LABEL", 1, InstructionType.SYNTHETIC),
+    ASSIGNMENT("ASSIGNMENT", 4, InstructionType.SYNTHETIC),
+    CONSTANT_ASSIGNMENT("CONSTANT_ASSIGNMENT", 2, InstructionType.SYNTHETIC),
+    JUMP_ZERO("JUMP_ZERO", 2, InstructionType.SYNTHETIC),
+    JUMP_EQUAL_CONSTANT("JUMP_EQUAL_CONSTANT", 2, InstructionType.SYNTHETIC),
+    JUMP_EQUAL_VARIABLE("JUMP_EQUAL_VARIABLE", 2, InstructionType.SYNTHETIC),
+    QUOTATION("QUOTATION", 5, InstructionType.SYNTHETIC),
+    JUMP_EQUAL_FUNCTION("JUMP_EQUAL_FUNCTION", 6, InstructionType.SYNTHETIC);
 
     private final String name;
     private final int cycles;
