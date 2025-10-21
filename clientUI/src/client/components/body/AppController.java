1cf89083317e67b844dc1055bc2d3baafcdeca35
Sean Berger <seanbe@mta.ac.il>
2025-10-21 13:40:32 +0300

    trying to sort out modules

--- clientUI/src/client/components/body/AppController.java
+++ clientUI/src/client/components/body/AppController.java
@@ -43,7 +43,6 @@ import org.jetbrains.annotations.Nullable;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.List;
