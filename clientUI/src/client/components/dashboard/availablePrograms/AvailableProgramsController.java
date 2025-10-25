0d80ead3b2dfa1f2535b1161cd411dc28c117e24
8anna8b6 <8anna8b6@gmail.com>
2025-10-25 14:03:31 +0200

    add dashboardheader.fxml and avaliableprograms.fxml

---
+++ clientUI/src/client/components/dashboard/availablePrograms/AvailableProgramsController.java
@@ -0,0 +1,4 @@
+package client.components.dashboard.availablePrograms;
+
+public class AvailableProgramsController {
+}
---
+++ clientUI/src/client/components/dashboard/dashboardHeader/DashboardHeaderController.java
@@ -0,0 +1,4 @@
+package client.components.dashboard.dashboardHeader;
+
+public class DashboardHeaderController {
+}
